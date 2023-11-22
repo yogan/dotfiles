aa41cf9c56189b124fd1ed5d1712eca01beb4107
Frank Blendinger <yogan@zogan.de>
2023-11-22 09:01:35 +0100

    nvim: remove explicit nvim-ts-context-commentstring setup
    Following the migration guide from:
    https://github.com/JoosepAlviste/nvim-ts-context-commentstring/issues/82#issuecomment-1817659634

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -1,12 +1,5 @@
 -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
 
-require 'nvim-treesitter.configs'.setup {
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    }
-}
-
 require('Comment').setup {
     pre_hook = require('ts_context_commentstring.integrations.comment_nvim')
         .create_pre_hook(),
