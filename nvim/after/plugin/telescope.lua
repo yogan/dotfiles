f2da8f6bcc4b5eb21b88e4ab340627663884b6ca
Frank Blendinger <yogan@zogan.de>
2023-12-18 20:36:29 +0100

    nvim: update telescope open file mappings (C-p, C-g)
    - Use <C-p> for find_files (current dir)
    - Use <C-g> for git_files (git repo)

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,18 +1,18 @@
-local telescope     = require('telescope')
-local action_layout = require('telescope.actions.layout')
-local builtin       = require('telescope.builtin')
+local telescope           = require('telescope')
+local action_layout       = require('telescope.actions.layout')
+local builtin             = require('telescope.builtin')
 
-local insert_mappings = {
+local insert_mappings     = {
     ['<C-h>']      = 'which_key', -- show mappings (same as <C-/>)
     ['<C-j>']      = 'move_selection_next',
     ['<C-k>']      = 'move_selection_previous',
-    ['<C-u>']      = false, -- remove to fallback to clear line
-    ['<PageUp>']   = 'preview_scrolling_up', -- replacements for
+    ['<C-u>']      = false,                    -- remove to fallback to clear line
+    ['<PageUp>']   = 'preview_scrolling_up',   -- replacements for
     ['<PageDown>'] = 'preview_scrolling_down', -- <C-u>/<C-d>
     ['<F4>']       = action_layout.toggle_preview,
 }
 
-local normal_mappings = {
+local normal_mappings     = {
     ['<F4>'] = action_layout.toggle_preview
 }
 
@@ -37,33 +37,33 @@ telescope.setup {
 telescope.load_extension('fzf')
 
 -- help
-vim.keymap.set('n', '<F1>', builtin.help_tags, { desc = 'Help tags' })
+vim.keymap.set('n', '<F1>',       builtin.help_tags, { desc = 'Help tags' })
 
 -- search
-vim.keymap.set('n', '<C-f>', builtin.live_grep) -- search prompt, regex on
-vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = 'Search for word under cursor' })
+vim.keymap.set('n', '<C-f>',      builtin.live_grep)
+vim.keymap.set('n', '<leader>*',  builtin.grep_string, { desc = 'Search for word under cursor' })
 
 -- buffers
-vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Buffers' })
+vim.keymap.set('n', '<C-b>',      builtin.buffers, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Buffers' })
 
 -- files
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find file' })
+vim.keymap.set('n', '<C-p>',      builtin.find_files, { desc = 'Find file' })
 
 -- git
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git files' })
+vim.keymap.set('n', '<C-g>',      builtin.git_files,  { desc = 'Git files' })
 vim.keymap.set('n', '<leader>pg', builtin.find_files, { desc = 'Git files' })
 vim.keymap.set('n', '<leader>pc', builtin.git_status, { desc = 'Git status (changes)' })
 
 -- vim things
 vim.keymap.set('n', '<leader>p/', builtin.search_history, { desc = 'Search history' })
-vim.keymap.set('n', '<leader>p=', builtin.spell_suggest, { desc = 'Spell suggestions' }) -- vim: z=
-vim.keymap.set('n', '<leader>ph', builtin.highlights, { desc = 'Highlights' })
-vim.keymap.set('n', '<leader>pj', builtin.jumplist, { desc = 'Jumplist (previous locations)' })
-vim.keymap.set('n', '<leader>pk', builtin.keymaps, { desc = 'Keymaps' })
-vim.keymap.set('n', '<leader>pm', builtin.marks, { desc = 'Marks' })
-vim.keymap.set('n', '<leader>po', builtin.vim_options, { desc = 'Options' })
-vim.keymap.set('n', '<leader>pr', builtin.registers, { desc = 'Registers' })
+vim.keymap.set('n', '<leader>p=', builtin.spell_suggest,  { desc = 'Spell suggestions' }) -- vim: z=
+vim.keymap.set('n', '<leader>ph', builtin.highlights,     { desc = 'Highlights' })
+vim.keymap.set('n', '<leader>pj', builtin.jumplist,       { desc = 'Jumplist (previous locations)' })
+vim.keymap.set('n', '<leader>pk', builtin.keymaps,        { desc = 'Keymaps' })
+vim.keymap.set('n', '<leader>pm', builtin.marks,          { desc = 'Marks' })
+vim.keymap.set('n', '<leader>po', builtin.vim_options,    { desc = 'Options' })
+vim.keymap.set('n', '<leader>pr', builtin.registers,      { desc = 'Registers' })
 
 -- symbols (telescope-symbols.nvim) (ps = symbols)
 vim.keymap.set('n', '<leader>pse', function() builtin.symbols { sources = { 'emoji' } } end,
