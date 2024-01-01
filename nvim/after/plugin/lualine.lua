7008145c855226546e9b3c4aa6fa18991b8fe532
Frank Blendinger <yogan@zogan.de>
2024-01-02 00:33:27 +0100

    nvim: show autoformat status in lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -6,10 +6,19 @@ local function session_name()
     return ' ' .. session.session_name
 end
 
+local function auto_format()
+	-- see :FormatEnable and :FormatDisable in conform.lua
+    if vim.b.disable_autoformat or vim.g.disable_autoformat then
+        return ''
+    end
+    return ''
+end
+
 require('lualine').setup {
     extensions = { 'nvim-tree', 'quickfix' },
     sections = {
         lualine_a = { session_name },
+        lualine_b = { auto_format },
         lualine_c = {
             {
                 'filename',
