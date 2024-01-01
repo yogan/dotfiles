72e270b551be620361141a2766188f9a140cce37
Frank Blendinger <yogan@zogan.de>
2024-01-02 00:04:49 +0100

    nvim: auto format lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,5 @@
-local lsp = require('lsp-zero')
-local lspconfig = require('lspconfig')
+local lsp = require("lsp-zero")
+local lspconfig = require("lspconfig")
 
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -13,91 +13,90 @@ require("mason-lspconfig").setup({
 	},
 })
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 
 lspconfig.lua_ls.setup({
 	-- Fix false positive undefined global '…'
-	settings = { Lua = { diagnostics = { globals = { "vim","P","RELOAD","R" } } } },
+	settings = { Lua = { diagnostics = { globals = { "vim", "P", "RELOAD", "R" } } } },
 })
 
 lspconfig.yamlls.setup({
-    -- https://github.com/redhat-developer/yaml-language-server#language-server-settings
-    settings = { yaml = { keyOrdering = false } }
+	-- https://github.com/redhat-developer/yaml-language-server#language-server-settings
+	settings = { yaml = { keyOrdering = false } },
 })
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
-local cmp_config = lsp.defaults.cmp_config {
-    sources = cmp.config.sources {
-        { name = 'nvim_lsp' }, -- lsp
-        { name = 'luasnip' }, -- snippets
-        { name = 'buffer' }, -- text within current buffer
-        { name = 'path' }, -- file system paths
-    }
-}
+local cmp_config = lsp.defaults.cmp_config({
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths
+	}),
+})
 
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings {
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm { select = true },
-    ['<C-Space>'] = cmp.mapping.complete(),
-}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
 
 -- Do NOT use <Return> to confirm the menu entry, but use <Ctrl><y> (see above)
 -- The reason for this is to allow abbreviation expansion with <Return>.
-cmp_mappings['<CR>'] = nil
+cmp_mappings["<CR>"] = nil
 
 -- Disable completion with <Tab>, those clash with GitHub Copilot.
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 
-lsp.setup_nvim_cmp {
-    config  = cmp_config,
-    mapping = cmp_mappings,
-}
+lsp.setup_nvim_cmp({
+	config = cmp_config,
+	mapping = cmp_mappings,
+})
 
 lsp.on_attach(function(client, bufnr)
-    -- for default key mappings, see:
-    -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#lsp-actions
-    lsp.default_keymaps({
-        buffer = bufnr,
-        preserve_mappings = false
-    })
+	-- for default key mappings, see:
+	-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#lsp-actions
+	lsp.default_keymaps({
+		buffer = bufnr,
+		preserve_mappings = false,
+	})
 
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+	local opts = { buffer = bufnr, remap = false }
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 
-    -- typescript specific keymaps (e.g. rename file and update imports)
-    if client.name == 'tsserver' then
-        vim.keymap.set('n', '<leader>rf', ':TypescriptRenameFile<CR>')
-        vim.keymap.set('n', '<leader>oi', ':TypescriptOrganizeImports<CR>')
-        vim.keymap.set('n', '<leader>ru', ':TypescriptRemoveUnused<CR>')
-    end
+	-- typescript specific keymaps (e.g. rename file and update imports)
+	if client.name == "tsserver" then
+		vim.keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")
+		vim.keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>")
+		vim.keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>")
+	end
 
-    -- highlight all references to symbol under cursor
-    if client.server_capabilities.documentHighlightProvider then
-        local LspDocumentHighlight = vim.api.nvim_create_augroup(
-            "lsp_document_highlight", { clear = true })
+	-- highlight all references to symbol under cursor
+	if client.server_capabilities.documentHighlightProvider then
+		local LspDocumentHighlight = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            callback = vim.lsp.buf.document_highlight,
-            group = LspDocumentHighlight,
-            buffer = bufnr,
-            desc = "Highlight symbol under cursor",
-        })
+		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+			callback = vim.lsp.buf.document_highlight,
+			group = LspDocumentHighlight,
+			buffer = bufnr,
+			desc = "Highlight symbol under cursor",
+		})
 
-        vim.api.nvim_create_autocmd("CursorMoved", {
-            callback = vim.lsp.buf.clear_references,
-            group = LspDocumentHighlight,
-            buffer = bufnr,
-            desc = "Clear old highlights",
-        })
-    end
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			callback = vim.lsp.buf.clear_references,
+			group = LspDocumentHighlight,
+			buffer = bufnr,
+			desc = "Clear old highlights",
+		})
+	end
 end)
 
 lsp.setup()
 
-vim.diagnostic.config {
-    virtual_text = true, -- show inline errors at the end of lines
-}
+vim.diagnostic.config({
+	virtual_text = true, -- show inline errors at the end of lines
+})
