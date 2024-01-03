f0e3a93deba0c4b2fcea68991ea1bf3e48f97aef
Frank Blendinger <yogan@zogan.de>
2024-01-03 21:18:41 +0100

    nvim: set indent as folding provider for yaml

--- nvim/after/plugin/ufo.lua
+++ nvim/after/plugin/ufo.lua
@@ -1,7 +1,18 @@
-require("ufo").setup()
+local ftMap = {
+	yaml = "indent",
+	-- add empty entry when a ft shall have no folding
+}
+
+---@diagnostic disable-next-line: missing-fields
+require("ufo").setup({
+	---@diagnostic disable-next-line: unused-local
+	provider_selector = function(bufnr, filetype, buftype)
+		return ftMap[filetype] -- default fallback is { "lsp", "indent" }
+	end,
+})
 
 vim.opt.foldcolumn = "1"
-vim.opt.foldlevel = 99 -- ufo provider need a large value
+vim.opt.foldlevel = 99 -- ufo provider needs a large value
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
