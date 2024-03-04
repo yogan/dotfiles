8c9827f08896e9283e8b626854adc0f68c782790
Frank Blendinger <yogan@zogan.de>
2024-03-04 23:48:53 +0100

    nvim: improve automatic cursorline and colorcolumn

--- nvim/lua/yogan/autocmds.lua
+++ nvim/lua/yogan/autocmds.lua
@@ -2,13 +2,6 @@
 -- this file without having to clear the autocmds first
 local group = vim.api.nvim_create_augroup("yogan-autocmds", { clear = true })
 
-vim.api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = { "TelescopePrompt", "help" },
-	desc = "Disable cursorline in TelescopePrompt and help",
-	command = "setlocal nocursorline",
-})
-
 vim.api.nvim_create_autocmd("VimResized", {
 	group = group,
 	desc = "Resize windows when terminal size changes",
@@ -33,13 +26,71 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	command = "setlocal spell",
 })
 
+local fully_ignore_fts = {
+	"noice",
+	"notify",
+}
+
+local no_cursorline_fts = {
+	"NvimTree",
+	"TelescopePrompt",
+	"TelescopeResults",
+	"WhichKey",
+	"help",
+	"neo-tree",
+}
+
+local no_colorcolumn_fts = {
+	"NvimTree",
+	"TelescopePrompt",
+	"TelescopeResults",
+	"Trouble",
+	"WhichKey",
+	"alpha",
+	"help",
+	"neo-tree",
+	"packer",
+	"qf",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = group,
+	desc = "Disable color column and cursor line for certain filetypes",
+	pattern = "*",
+	callback = function()
+		if vim.tbl_contains(fully_ignore_fts, vim.bo.filetype) then
+			return
+		end
+
+		if vim.tbl_contains(no_cursorline_fts, vim.bo.filetype) then
+			vim.opt_local.cursorline = false
+
+		else
+			vim.opt_local.cursorline = true
+		end
+		if vim.tbl_contains(no_colorcolumn_fts, vim.bo.filetype) then
+			vim.opt_local.colorcolumn = "0"
+		else
+			vim.opt_local.colorcolumn = "+1,+20"
+		end
+	end,
+})
+
 vim.api.nvim_create_autocmd("WinEnter", {
 	group = group,
 	desc = "Enable color column and cursor line for active window",
 	pattern = "*",
 	callback = function()
-		vim.opt.colorcolumn = "+1,+20"
-		vim.opt.cursorline = true
+		if vim.tbl_contains(fully_ignore_fts, vim.bo.filetype) then
+			return
+		end
+
+		if not vim.tbl_contains(no_cursorline_fts, vim.bo.filetype) then
+			vim.opt_local.cursorline = true
+		end
+		if not vim.tbl_contains(no_colorcolumn_fts, vim.bo.filetype) then
+			vim.opt_local.colorcolumn = "+1,+20"
+		end
 	end,
 })
 
@@ -48,8 +99,12 @@ vim.api.nvim_create_autocmd("WinLeave", {
 	desc = "Disable color column and cursor line for inactive windows",
 	pattern = "*",
 	callback = function()
-		vim.opt.colorcolumn = "0"
-		vim.opt.cursorline = false
+		if vim.tbl_contains(fully_ignore_fts, vim.bo.filetype) then
+			return
+		end
+
+		vim.opt_local.cursorline = false
+		vim.opt_local.colorcolumn = "0"
 	end,
 })
 
