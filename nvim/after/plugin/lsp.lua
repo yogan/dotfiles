e5752feb4b8b908a5eb7e9634071fe8bff8f7251
Frank Blendinger <yogan@zogan.de>
2024-08-21 07:56:52 +0200

    nvim: remove debug message in Julia LSP setup

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -71,7 +71,6 @@ lspconfig.julials.setup({
 	on_new_config = function(new_config, _)
 		local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
 		if require("lspconfig").util.path.is_file(julia) then
-			vim.notify("Hello!")
 			new_config.cmd[1] = julia
 		end
 	end,
