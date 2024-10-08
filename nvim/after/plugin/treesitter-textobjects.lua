842456bd7035d9e702b5b102a3a90aa26a3bc9d7
Frank Blendinger <yogan@zogan.de>
2024-10-08 17:11:45 +0200

    nvim: add repeatable treesitter textobject movements

--- nvim/after/plugin/treesitter-textobjects.lua
+++ nvim/after/plugin/treesitter-textobjects.lua
@@ -24,5 +24,38 @@ require("nvim-treesitter.configs").setup({
 				["a/"] = { query = "@comment.outer", desc = "Select outer comment" },
 			},
 		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = { query = "@function.outer", desc = "Next function start" },
+				["]]"] = { query = "@class.outer", desc = "Next class start" },
+			},
+			goto_next_end = {
+				["]M"] = { query = "@function.outer", desc = "Next function end" },
+				["]["] = { query = "@class.outer", desc = "Next class end" },
+			},
+			goto_previous_start = {
+				["[m"] = { query = "@function.outer", desc = "Previous function start" },
+				["[["] = { query = "@class.outer", desc = "Previous class start" },
+			},
+			goto_previous_end = {
+				["[M"] = { query = "@function.outer", desc = "Previous function end" },
+				["[]"] = { query = "@class.outer", desc = "Previous class end" },
+			},
+		},
 	},
 })
+
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+-- Repeat movement with ; and ,
+-- ensure ; goes forward and , goes backward regardless of the last direction
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { desc = "Repeat last movement (forward)" })
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { desc = "Repeat last movement (backward)" })
+
+-- Make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
