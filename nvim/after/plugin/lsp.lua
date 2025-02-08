6995437bdd66aa5f605eca0e8abdd043a85363f7
Frank Blendinger <yogan@zogan.de>
2025-02-08 23:27:03 +0100

    nvim: use icons for LSP diagnostics in gutter

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -197,3 +197,11 @@ lsp_zero.setup()
 vim.diagnostic.config({
 	virtual_text = true, -- show inline errors at the end of lines
 })
+
+-- Diagnostic icons in gutter, see:
+-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
