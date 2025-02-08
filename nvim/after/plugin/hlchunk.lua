978ddc45745cf412462e61a58913de8974512ab5
Frank Blendinger <yogan@zogan.de>
2025-02-08 23:11:10 +0100

    nvim: tweak hlchung color

--- nvim/after/plugin/hlchunk.lua
+++ nvim/after/plugin/hlchunk.lua
@@ -1,14 +1,23 @@
 require("hlchunk").setup({
 	chunk = {
 		enable = true,
+		style = {
+			{ fg = "#8e3434" }, -- normal chunk
+			{ fg = "#e11811" }, -- chunk with error(s)
+		},
 	},
 	indent = {
 		enable = true,
+		filter_list = {
+			function(v)
+				return v.level ~= 1
+			end,
+		},
 		style = {
-			{ fg = "#434437" },
-			{ fg = "#2f4440" },
-			{ fg = "#433054" },
-			{ fg = "#284251" },
+			{ fg = "#351f1e" },
+			{ fg = "#3c1a19" },
+			{ fg = "#421411" },
+			{ fg = "#4d0706" },
 		},
 	},
 })
