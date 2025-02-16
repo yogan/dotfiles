2a4f8d52228678e5468c6c986e073f41f246e28a
Frank Blendinger <yogan@zogan.de>
2025-02-16 23:06:50 +0100

    nvim: color scheme tweaks

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,6 +8,7 @@ require("ashen").setup({
 			EndOfBuffer = { fg = "#424242", bg = "#0c0c0c" },
 			ExtraWhitespace = { bg = "#562727" },
 			Folded = { bg = "#181818" },
+			WhichKeyGroup = { "g_7", { italic = true } },
 		},
 		link = {
 			MoreMsg = "AshenGreenLight",
@@ -18,3 +19,7 @@ require("ashen").setup({
 })
 
 vim.cmd("colorscheme ashen")
+
+-- Putting this in hl.link doesn't work, even like this:
+-- ["@markup.raw"] = "@markup.italic"
+vim.cmd("highlight! link @markup.raw @markup.italic")
