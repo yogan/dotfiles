704b00cd4396ef366dba25effc3e16f2c106292d
Frank Blendinger <yogan@zogan.de>
2025-02-21 00:20:08 +0100

    nvim: add startup section to bottom of dashboard

--- nvim/after/plugin/snacks.lua
+++ nvim/after/plugin/snacks.lua
@@ -53,7 +53,7 @@ require("snacks").setup({
 			{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
 			{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-			-- { section = "startup" },
+			{ section = "startup" },
 		},
 	},
 	words = { enabled = true },
