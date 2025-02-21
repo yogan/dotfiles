9230d357e6e1bad8fb8fb7a61e6895d271feaf83
Frank Blendinger <yogan@zogan.de>
2025-02-21 19:18:10 +0100

    nvim: inline better-whitespace config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -25,6 +25,28 @@ return {
 		-- has its filetype set to 'snacks_dashboard', see:
 		-- https://github.com/folke/snacks.nvim/issues/1220#issuecomment-2661542968
 		event = "BufReadPost",
+		init = function()
+			-- Strip whitespace operator, also works on visual selection
+			-- Operator mode examples:
+			--    <leader>Wj  - current line
+			--    <leader>Wap - current paragraph
+			--    <leader>Wib - current block (e.g. {…})
+			--    <leader>Wig - current git change hunk
+			vim.g.better_whitespace_operator = "<leader>W"
+
+			vim.g.better_whitespace_filetypes_blacklist = {
+				"diff",
+				"git",
+				"gitcommit",
+				"unite",
+				"qf",
+				"help",
+				"markdown",
+				"fugitive",
+				"snacks_dashboard",
+				"",
+			}
+		end,
 	},
 
 	-- GitHub Copilot
