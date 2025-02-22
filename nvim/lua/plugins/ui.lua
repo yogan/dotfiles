835269d549c0ee6cb72e3f382351e4f277fd662a
Frank Blendinger <yogan@zogan.de>
2025-02-23 00:05:53 +0100

    nvim: inline noice config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -25,7 +25,27 @@ return {
 	},
 
 	-- Fancy command line / search bar / messages
-	{ "folke/noice.nvim", dependencies = { "MunifTanjim/nui.nvim" } },
+	{
+		"folke/noice.nvim",
+		dependencies = "MunifTanjim/nui.nvim",
+		opts = {
+			message = { enabled = false },
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		},
+	},
 
 	-- Automatically clear search highlights after moving away from them
 	"romainl/vim-cool",
