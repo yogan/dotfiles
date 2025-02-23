e10cff4fe67fe8eeb8a5a46ba77757d36483f1b9
Frank Blendinger <yogan@zogan.de>
2025-02-23 11:54:21 +0100

    nvim: inline statuscol config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -9,9 +9,35 @@ return {
 	{
 		"kevinhwang91/nvim-ufo",
 		dependencies = {
-			"kevinhwang91/promise-async",
-			"luukvbaal/statuscol.nvim",
 			"folke/which-key.nvim",
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{
+								sign = { namespace = { "gitsign" }, auto = true },
+								click = "v:lua.ScSa",
+							},
+							{
+								sign = { namespace = { "diagnostic*" }, auto = true },
+								click = "v:lua.ScSa",
+							},
+							{
+								text = { builtin.foldfunc, " " },
+								click = "v:lua.ScFa",
+							},
+							{
+								text = { builtin.lnumfunc, " " },
+								click = "v:lua.ScLa",
+							},
+						},
+					})
+				end,
+			},
 		},
 		config = function()
 			local ufo = require("ufo")
@@ -22,6 +48,7 @@ return {
 				-- add empty entry when a ft shall have no folding
 			}
 
+			---@diagnostic disable-next-line: missing-fields
 			ufo.setup({
 				provider_selector = function(_, filetype)
 					return ftMap[filetype] -- default fallback is { "lsp", "indent" }
