4867de26bd53a6165856d00b6f77eefa05cdbcb6
Frank Blendinger <yogan@zogan.de>
2025-02-25 10:21:05 +0100

    nvim: add dial.nvim for better ^A/^X

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -103,6 +103,47 @@ return {
 		end,
 	},
 
+	-- Extended increment/decrement (^A/^X), works for dates, hex colors, etc.
+	{
+		"monaqa/dial.nvim",
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.hexcolor.new({ case = "lower" }),
+				},
+			})
+
+			vim.keymap.set("n", "<C-a>", function()
+				require("dial.map").manipulate("increment", "normal")
+			end)
+			vim.keymap.set("n", "<C-x>", function()
+				require("dial.map").manipulate("decrement", "normal")
+			end)
+			vim.keymap.set("n", "g<C-a>", function()
+				require("dial.map").manipulate("increment", "gnormal")
+			end)
+			vim.keymap.set("n", "g<C-x>", function()
+				require("dial.map").manipulate("decrement", "gnormal")
+			end)
+			vim.keymap.set("v", "<C-a>", function()
+				require("dial.map").manipulate("increment", "visual")
+			end)
+			vim.keymap.set("v", "<C-x>", function()
+				require("dial.map").manipulate("decrement", "visual")
+			end)
+			vim.keymap.set("v", "g<C-a>", function()
+				require("dial.map").manipulate("increment", "gvisual")
+			end)
+			vim.keymap.set("v", "g<C-x>", function()
+				require("dial.map").manipulate("decrement", "gvisual")
+			end)
+		end,
+	},
+
 	-- Just for fun
 	"eandrju/cellular-automaton.nvim",
 }
