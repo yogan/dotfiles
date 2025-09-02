29f17335ddf33b8f3789abe0a8ac38c7f254d758
Frank Blendinger <yogan@zogan.de>
2025-09-02 08:56:02 +0200

    nvim: lazy plugins update

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -111,10 +111,10 @@ return {
 			vim.keymap.set("n", "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end)
 			vim.keymap.set("n", "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end)
 			vim.keymap.set("n", "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end)
-			vim.keymap.set("v", "<C-a>", function() require("dial.map").manipulate("increment", "visual") end)
-			vim.keymap.set("v", "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end)
-			vim.keymap.set("v", "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end)
-			vim.keymap.set("v", "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end)
+			vim.keymap.set("x", "<C-a>", function() require("dial.map").manipulate("increment", "visual") end)
+			vim.keymap.set("x", "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end)
+			vim.keymap.set("x", "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end)
+			vim.keymap.set("x", "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end)
 		end,
 	},
 
