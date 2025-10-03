04dfd7f0301388f5091973fae3f1674afbb1cbfc
Frank Blendinger <yogan@zogan.de>
2025-10-03 10:55:28 +0200

    nvim(lsp,lua): set array index inline hints to "auto"

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -35,7 +35,11 @@ local function setup_lspconfig()
 				-- See https://github.com/LuaLS/lua-language-server/wiki/Settings#hint
 				hint = {
 					enable = true,
-					arrayIndex = "Disable",
+
+					-- "Enable"  - Show hint in all tables
+					-- "Auto"    - Only show hint when there is more than 3 items or the table is mixed (indexes and keys)
+					-- "Disable" - Disable array index hints
+					arrayIndex = "Auto",
 				},
 			},
 		},
